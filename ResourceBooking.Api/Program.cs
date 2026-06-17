3c0b92d21bc6af11532ee3703a48ceef3bdff82a
markjohn516 <49624966+markjohn516@users.noreply.github.com>
2026-06-17 15:37:08 +0800

    Register repositories and services

--- ResourceBooking.Api/Program.cs
+++ ResourceBooking.Api/Program.cs
@@ -1,41 +1,62 @@
+using Microsoft.EntityFrameworkCore;
+using ResourceBooking.Api.Data;
+using ResourceBooking.Api.Repositories;
+using ResourceBooking.Api.Services;
+using ResourceBooking.Core.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Add services to the container
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+// Configure CORS for all origins
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+// Configure Entity Framework
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? "Data Source=resourcebooking.db";
+
+builder.Services.AddDbContext<ResourceBookingContext>(options =>
+    options.UseSqlite(connectionString));
+
+// Register repositories
+builder.Services.AddScoped<IRepository<Employee>, Repository<Employee>>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+
+// Register services
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IResourceService, ResourceService>();
+builder.Services.AddScoped<IBookingService, BookingService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Migrate database on startup
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ResourceBookingContext>();
+    context.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Disable HTTPS redirection in development for stability
+// app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
