17e4318b0fe77966a5eee3cc9106c3ee2a833b81
markjohn516 <49624966+markjohn516@users.noreply.github.com>
2026-06-17 15:51:50 +0800

    Register api client

--- ResourceBooking.Blazor/Program.cs
+++ ResourceBooking.Blazor/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ResourceBooking.Blazor;
+using ResourceBooking.Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000") });
+builder.Services.AddScoped<IApiClient, ApiClient>();
+
+builder.Services.AddScoped(sp =>
+{
+    var logger = sp.GetRequiredService<ILogger<ApiClient>>();
+    return new ApiClient(new HttpClient { BaseAddress = new Uri("https://localhost:7000") }, logger);
+});
 
 await builder.Build().RunAsync();
